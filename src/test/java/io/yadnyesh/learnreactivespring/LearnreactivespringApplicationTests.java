3a255de91eea15be06650fb2e98a1a08d70e1740
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-06-02 11:25:04 +0530

    First Commit

---
+++ src/test/java/io/yadnyesh/learnreactivespring/LearnreactivespringApplicationTests.java
@@ -0,0 +1,16 @@
+package io.yadnyesh.learnreactivespring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearnreactivespringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
