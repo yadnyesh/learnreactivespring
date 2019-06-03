b5ab31977ac3549405aaf25b1fb4f139c5c3b82b
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-06-03 09:37:55 +0530

    Flux - onErrorResume

---
+++ src/test/java/io/yadnyesh/learnreactivespring/fluxandmonoplayground/FluxAndMonoErrorTest.java
@@ -0,0 +1,30 @@
+package io.yadnyesh.learnreactivespring.fluxandmonoplayground;
+
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+public class FluxAndMonoErrorTest
+{
+
+    @Test
+    public void fluxErrorHandling() {
+        Flux stringFlux = Flux.just("A", "B", "C")
+                .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
+                .concatWith(Flux.just("D"))
+                .onErrorResume((e) -> {
+                    System.out.println("Exception is : " + e);
+                    return Flux.just("default","default1" );
+                });
+
+        StepVerifier.create(stringFlux.log())
+                .expectSubscription()
+                .expectNext("A", "B", "C")
+//                .expectError(RuntimeException.class)
+//                .verify();
+                .expectNext("default","default1")
+                .verifyComplete();
+    }
+
+}
+
