2ce6ae540ad4473ed357d5c9e5bd5b394aff6670
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-06-03 08:38:10 +0530

    Flux - combineUsingMerge_withConcat

--- src/test/java/io/yadnyesh/learnreactivespring/fluxandmonoplayground/FluxAndMonoCombineTest.java
+++ src/test/java/io/yadnyesh/learnreactivespring/fluxandmonoplayground/FluxAndMonoCombineTest.java
@@ -34,4 +34,18 @@ public class FluxAndMonoCombineTest {
                 //.expectNext("A", "B", "C", "D", "E", "F")
                 .verifyComplete();
     }
+
+    @Test
+    public void combineUsingMerge_withConcat() {
+        Flux<String> flux1 = Flux.just("A", "B", "C").delayElements(Duration.ofSeconds(1));
+        Flux<String> flux2 = Flux.just("D", "E", "F");
+
+        Flux<String> mergedFlux = Flux.concat(flux1,flux2);
+
+        StepVerifier.create(mergedFlux.log())
+                .expectSubscription()
+                //.expectNextCount(6)
+                .expectNext("A", "B", "C", "D", "E", "F")
+                .verifyComplete();
+    }
 }
