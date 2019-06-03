9f5f17bd3d139584e2205bd92b03071851fdefb1
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-06-03 15:34:14 +0530

    Flux - InfiniteSequenceTest, InfiniteSequenceTest_WithMap

--- src/test/java/io/yadnyesh/learnreactivespring/fluxandmonoplayground/FluxAndMonoWithTimeTest.java
+++ src/test/java/io/yadnyesh/learnreactivespring/fluxandmonoplayground/FluxAndMonoWithTimeTest.java
@@ -2,6 +2,7 @@ package io.yadnyesh.learnreactivespring.fluxandmonoplayground;
 
 import org.junit.Test;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 import java.time.Duration;
 
@@ -14,5 +15,28 @@ public class FluxAndMonoWithTimeTest {
         infiniteFlux.subscribe((e) -> System.out.println("Current Element is: " + e));
 
         Thread.sleep(3000);
+    }
+
+    @Test
+    public void InfiniteSequenceTest() throws InterruptedException {
+        Flux<Long> infiniteFlux = Flux.interval(Duration.ofMillis(200))
+                .take(3)
+                .log();
+        StepVerifier.create(infiniteFlux)
+                .expectSubscription()
+                .expectNext(0L, 1L, 2L)
+                .verifyComplete();
+    }
+
+    @Test
+    public void InfiniteSequenceTest_WithMap() throws InterruptedException {
+        Flux<Integer> infiniteFlux = Flux.interval(Duration.ofMillis(200))
+                .map(l -> new Integer(l.intValue()))
+                .take(3)
+                .log();
+        StepVerifier.create(infiniteFlux)
+                .expectSubscription()
+                .expectNext(0, 1, 2)
+                .verifyComplete();
     }
 }
