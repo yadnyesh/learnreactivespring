49d87137b7b1dd5399a4fb7eaf7bc2aba91a007e
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-06-03 08:36:46 +0530

    Flux - combineUsingMerge_withDelay

---
+++ src/test/java/io/yadnyesh/learnreactivespring/fluxandmonoplayground/FluxAndMonoCombineTest.java
@@ -0,0 +1,37 @@
+package io.yadnyesh.learnreactivespring.fluxandmonoplayground;
+
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+public class FluxAndMonoCombineTest {
+
+    @Test
+    public void combineUsingMerge() {
+        Flux<String> flux1 = Flux.just("A", "B", "C");
+        Flux<String> flux2 = Flux.just("A", "B", "C");
+
+        Flux<String> mergedFlux = Flux.merge(flux1,flux2);
+
+        StepVerifier.create(mergedFlux.log())
+                .expectSubscription()
+                .expectNext("A", "B", "C", "A", "B", "C")
+                .verifyComplete();
+    }
+
+    @Test
+    public void combineUsingMerge_withDelay() {
+        Flux<String> flux1 = Flux.just("A", "B", "C").delayElements(Duration.ofSeconds(1));
+        Flux<String> flux2 = Flux.just("D", "E", "F");
+
+        Flux<String> mergedFlux = Flux.merge(flux1,flux2);
+
+        StepVerifier.create(mergedFlux.log())
+                .expectSubscription()
+                .expectNextCount(6)
+                //.expectNext("A", "B", "C", "D", "E", "F")
+                .verifyComplete();
+    }
+}
