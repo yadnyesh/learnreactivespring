8282a381b3eac4ba46beb5004457b5d56c71b9b4
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-06-04 10:42:40 +0530

    Flux - Testing Flux Controller - getResponseBody

--- src/test/java/io/yadnyesh/learnreactivespring/controller/FluxAndMonoControllerTest.java
+++ src/test/java/io/yadnyesh/learnreactivespring/controller/FluxAndMonoControllerTest.java
@@ -1,14 +1,19 @@
 package io.yadnyesh.learnreactivespring.controller;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @WebFluxTest
@@ -48,5 +53,21 @@ public class FluxAndMonoControllerTest {
                 .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
                 .expectBodyList(Integer.class)
                 .hasSize(4);
+    }
+
+    @Test
+    public void flux_approach3() {
+
+        List<Integer> expectedList = Arrays.asList(1,2,3,4);
+
+        EntityExchangeResult<List<Integer>> entityExchangeResult = webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                //.expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Integer.class)
+                .returnResult();
+
+        Assert.assertEquals(expectedList, entityExchangeResult.getResponseBody());
     }
 }
