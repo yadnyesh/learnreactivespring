bf5c7bfdbc09dda18ce6f2849edc38af9b651997
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-06-04 09:10:14 +0530

    Flux - testingWithVirtualTime

--- src/test/java/io/yadnyesh/learnreactivespring/fluxandmonoplayground/VirtualTimeTest.java
+++ src/test/java/io/yadnyesh/learnreactivespring/fluxandmonoplayground/VirtualTimeTest.java
@@ -3,6 +3,7 @@ package io.yadnyesh.learnreactivespring.fluxandmonoplayground;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+import reactor.test.scheduler.VirtualTimeScheduler;
 
 import java.time.Duration;
 
@@ -16,5 +17,18 @@ public class VirtualTimeTest {
                 .expectNext(0L,1L, 2L)
                 .verifyComplete();
     }
+
+    @Test
+    public void testingWithVirtualTime() {
+        VirtualTimeScheduler.getOrSet();
+        Flux<Long> longFlux = Flux.interval(Duration.ofSeconds(1)).take(3);
+        StepVerifier.withVirtualTime(() -> longFlux.log())
+                .expectSubscription()
+                .thenAwait(Duration.ofSeconds(3))
+                .expectNext(0L,1L, 2L)
+                .verifyComplete();
+
+    }
+
 
 }
