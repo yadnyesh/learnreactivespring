806dcae8563290e4d4aa292762ff648e5301408c
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-06-04 08:47:14 +0530

    Flux - hotPublisherTest() ConnectableFlux<String>

--- src/test/java/io/yadnyesh/learnreactivespring/fluxandmonoplayground/ColdAndHotPublisherTest.java
+++ src/test/java/io/yadnyesh/learnreactivespring/fluxandmonoplayground/ColdAndHotPublisherTest.java
@@ -1,6 +1,7 @@
 package io.yadnyesh.learnreactivespring.fluxandmonoplayground;
 
 import org.junit.Test;
+import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
@@ -16,6 +17,18 @@ public class ColdAndHotPublisherTest {
         stringFlux.subscribe(s -> System.out.println("Subscriber 1: " + s));
         Thread.sleep(2000);
         stringFlux.subscribe(s -> System.out.println("Subscriber 2: " + s));
+        Thread.sleep(4000);
+    }
+
+    @Test
+    public void hotPublisherTest() throws InterruptedException {
+        Flux<String> stringFlux = Flux.just("A", "B", "C", "D", "E", "F")
+                .delayElements(Duration.ofSeconds(1));
+        ConnectableFlux<String> connectableFlux = stringFlux.publish();
+        connectableFlux.connect();
+        connectableFlux.subscribe(s -> System.out.println("Subscriber 1: " + s));
+        Thread.sleep(3000);
+        connectableFlux.subscribe(s -> System.out.println("Subscriber 2: " + s));
         Thread.sleep(4000);
     }
 }
