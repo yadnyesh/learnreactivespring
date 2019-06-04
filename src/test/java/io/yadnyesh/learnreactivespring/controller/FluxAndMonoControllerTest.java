6c360e2556d1b60e3c02ded32b08e89714700055
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-06-04 10:29:02 +0530

    Flux - Testing Flux Controller - Option1

---
+++ src/test/java/io/yadnyesh/learnreactivespring/controller/FluxAndMonoControllerTest.java
@@ -0,0 +1,40 @@
+package io.yadnyesh.learnreactivespring.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+@RunWith(SpringRunner.class)
+@WebFluxTest
+public class FluxAndMonoControllerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    public void flux_approach1() {
+       Flux<Integer> integerFlux = webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()//Actually makes the call
+                .expectStatus()
+                .isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+       StepVerifier.create(integerFlux.log())
+                .expectSubscription()
+                .expectNext(1)
+                .expectNext(2)
+                .expectNext(3)
+                .expectNext(4)
+                .verifyComplete();
+
+
+    }
+}
