8e98655b616043af2a1d0be8eb40e93395351f4e
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-06-05 10:53:19 +0530

    Flux And Mono - RouteHandler and FunctionHandler - Controller and Test

---
+++ src/test/java/io/yadnyesh/learnreactivespring/handler/SampleHandlerFunctionTest.java
@@ -0,0 +1,42 @@
+package io.yadnyesh.learnreactivespring.handler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureWebTestClient
+public class SampleHandlerFunctionTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    public void flux_approach1() {
+        Flux<Integer> integerFlux = webTestClient.get().uri("/functional/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()//Actually makes the call
+                .expectStatus()
+                .isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(integerFlux.log())
+                .expectSubscription()
+                .expectNext(1)
+                .expectNext(2)
+                .expectNext(3)
+                .expectNext(4)
+                .verifyComplete();
+
+
+    }
+}
