964d2987829035c6a97b2e07cbf6c40ea54d2026
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-06-05 18:40:23 +0530

    Flux And Mono - RouteHandler and FunctionHandler - Controller and Test -- Look at the Mono response this should be further investigated.

--- src/test/java/io/yadnyesh/learnreactivespring/handler/SampleHandlerFunctionTest.java
+++ src/test/java/io/yadnyesh/learnreactivespring/handler/SampleHandlerFunctionTest.java
@@ -1,5 +1,6 @@
 package io.yadnyesh.learnreactivespring.handler;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyExtractors;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
@@ -38,5 +40,20 @@ public class SampleHandlerFunctionTest {
                 .verifyComplete();
 
 
+    }
+
+    @Test
+    public void mono() {
+
+        Integer expectedValue = new Integer(1);
+
+        webTestClient.get().uri("/functional/mono")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Integer[].class)
+                .consumeWith((response) -> {
+                    Assert.assertEquals(expectedValue, response.getResponseBody()[0]);
+                });
     }
 }
