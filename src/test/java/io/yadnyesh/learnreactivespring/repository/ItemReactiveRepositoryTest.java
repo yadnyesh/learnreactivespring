c76a59e233e8a95cd229a68baafe2bdba66d33fb
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-06-06 10:28:25 +0530

    Embedded MongoDB, DB Setup, Test for getItemById

--- src/test/java/io/yadnyesh/learnreactivespring/repository/ItemReactiveRepositoryTest.java
+++ src/test/java/io/yadnyesh/learnreactivespring/repository/ItemReactiveRepositoryTest.java
@@ -20,7 +20,8 @@ public class ItemReactiveRepositoryTest {
     List<Item> itemList = Arrays.asList(new Item(null, "Samsung TV", 400.0),
             new Item(null, "LG TV", 500.0),
             new Item(null, "Apple Watch", 299.99),
-            new Item(null, "Beats Headphones", 149.99));
+            new Item(null, "Beats Headphones", 149.99),
+            new Item("ABC", "Bose Headphones", 149.99));
 
     @Autowired
     ItemReactiveRepository itemReactiveRepository;
@@ -41,7 +42,15 @@ public class ItemReactiveRepositoryTest {
 
         StepVerifier.create(itemReactiveRepository.findAll())
                 .expectSubscription()
-                .expectNextCount(4)
+                .expectNextCount(5)
+                .verifyComplete();
+    }
+
+    @Test
+    public void getItemById() {
+        StepVerifier.create(itemReactiveRepository.findById("ABC"))
+                .expectSubscription()
+                .expectNextMatches(item -> item.getDescription().equals("Bose Headphones"))
                 .verifyComplete();
     }
 }
