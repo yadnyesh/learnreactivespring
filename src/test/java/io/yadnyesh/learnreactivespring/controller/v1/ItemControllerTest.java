74dd592914f7d3d4fc8fe8c137ff83aa5161c5e9
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-06-07 22:30:06 +0530

    Reactive Mongo - Create asynchronous & non-blocking Delete endpoint unit test deleteItem()

--- src/test/java/io/yadnyesh/learnreactivespring/controller/v1/ItemControllerTest.java
+++ src/test/java/io/yadnyesh/learnreactivespring/controller/v1/ItemControllerTest.java
@@ -126,4 +126,14 @@ public class ItemControllerTest {
 
     }
 
+    @Test
+    public void deleteItem() {
+        webTestClient.delete().uri(ItemConstants.ITEM_END_POINT_V1.concat("/{id}"), "ABC")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Void.class);
+
+    }
+
 }
