5d373f50517143212c3b1d6cd097cadf80fe55b4
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-06-07 08:55:34 +0530

    Reactive Mongo - Get All Items -- With Test Profile

--- src/main/java/io/yadnyesh/learnreactivespring/initialize/ItemDataInitializer.java
+++ src/main/java/io/yadnyesh/learnreactivespring/initialize/ItemDataInitializer.java
@@ -4,6 +4,7 @@ import io.yadnyesh.learnreactivespring.domain.Item;
 import io.yadnyesh.learnreactivespring.repository.ItemReactiveRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
@@ -11,6 +12,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Component
+@Profile("!test")
 public class ItemDataInitializer implements CommandLineRunner {
 
     @Autowired
---
+++ src/test/java/io/yadnyesh/learnreactivespring/controller/v1/ItemControllerTest.java
@@ -0,0 +1,62 @@
+package io.yadnyesh.learnreactivespring.controller.v1;
+
+import io.yadnyesh.learnreactivespring.constants.ItemConstants;
+import io.yadnyesh.learnreactivespring.domain.Item;
+import io.yadnyesh.learnreactivespring.repository.ItemReactiveRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@DirtiesContext
+@AutoConfigureWebTestClient
+@ActiveProfiles("test")
+public class ItemControllerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    public List<Item> data() {
+        return Arrays.asList(new Item(null, "Samsung TV", 400.0),
+                new Item(null, "LG TV", 500.0),
+                new Item(null, "Apple Watch", 299.99),
+                new Item(null, "Beats Headphones", 149.99),
+                new Item("ABC", "Bose Headphones", 149.99));
+    }
+
+    @Before
+    public void setup() {
+        itemReactiveRepository.deleteAll().thenMany(Flux.fromIterable(data()))
+                .flatMap(itemReactiveRepository::save)
+                .doOnNext(item -> {
+                    System.out.println("Inserted item is: " + item);
+                })
+                .blockLast();
+    }
+
+    @Test
+    public void getAllItems() {
+        webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Item.class)
+                .hasSize(5);
+    }
+}
