2dbea2e293e94d8f126315036794d731f3ba1b96
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-06-08 09:56:53 +0530

    Reactive Mongo - Create asynchronous & non-blocking Update endpoint unit test updateItem()

--- src/main/java/io/yadnyesh/learnreactivespring/controller/v1/ItemController.java
+++ src/main/java/io/yadnyesh/learnreactivespring/controller/v1/ItemController.java
@@ -41,4 +41,15 @@ public class ItemController {
     public Mono<Void> deleteItem(@PathVariable String id) {
         return itemReactiveRepository.deleteById(id);
     }
+
+    @PutMapping(ItemConstants.ITEM_END_POINT_V1 + "/{id}")
+    public Mono<ResponseEntity<Item>> updateItem(@PathVariable String id, @RequestBody Item item) {
+        return itemReactiveRepository.findById(id)
+                                    .flatMap((presentItem) -> {
+                                        presentItem.setPrice(item.getPrice());
+                                        presentItem.setDescription(item.getDescription());
+                                        return itemReactiveRepository.save(presentItem);
+                                    }).map(updatedItem -> new ResponseEntity<>(updatedItem, HttpStatus.OK))
+                                    .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 }
