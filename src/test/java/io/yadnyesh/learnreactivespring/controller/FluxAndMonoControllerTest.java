6ba0546b1a4f1f4e62436853939df3ffc77947e0
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-06-08 18:57:41 +0530

    To Scan all the components - WebFluxTest Does not scan all the beans, so use @SpringBootTest and @AutoConfigureWebTestClient

--- src/test/java/io/yadnyesh/learnreactivespring/controller/FluxAndMonoControllerTest.java
+++ src/test/java/io/yadnyesh/learnreactivespring/controller/FluxAndMonoControllerTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
