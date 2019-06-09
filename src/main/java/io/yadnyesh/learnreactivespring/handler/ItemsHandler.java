99b9debfc9bcf832c14b366ed6720790f81aa7be
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-06-09 13:02:50 +0530

    Router and Handler Function - Get All items endpoint

--- src/main/java/io/yadnyesh/learnreactivespring/handler/ItemsHandler.java
+++ src/main/java/io/yadnyesh/learnreactivespring/handler/ItemsHandler.java
@@ -1,12 +1,24 @@
 package io.yadnyesh.learnreactivespring.handler;
 
+import io.yadnyesh.learnreactivespring.domain.Item;
 import io.yadnyesh.learnreactivespring.repository.ItemReactiveRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
 @Component
 public class ItemsHandler {
 
     @Autowired
     ItemReactiveRepository itemReactiveRepository;
+
+    public Mono<ServerResponse> getAllItems(ServerRequest serverRequest){
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .body(itemReactiveRepository.findAll(), Item.class);
+
+    }
 }
--- src/main/java/io/yadnyesh/learnreactivespring/router/ItemsRouter.java
+++ src/main/java/io/yadnyesh/learnreactivespring/router/ItemsRouter.java
@@ -1,17 +1,27 @@
 package io.yadnyesh.learnreactivespring.router;
 
+import io.yadnyesh.learnreactivespring.constants.ItemConstants;
 import io.yadnyesh.learnreactivespring.handler.ItemsHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 
 @Configuration
 public class ItemsRouter {
 
     @Bean
     public RouterFunction<ServerResponse> itemsRoute (ItemsHandler itemsHandler) {
-        return null;
-    }
 
+        return RouterFunctions.route(GET(ItemConstants.ITEM_FUNCTIONAL_END_POINT_V1)
+                                .and(accept(MediaType.APPLICATION_JSON_UTF8))
+                                ,itemsHandler::getAllItems);
+    }
 }
