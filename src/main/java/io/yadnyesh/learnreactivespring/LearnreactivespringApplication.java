93a553d09c02e239152a1e12d0539464715b0bc0
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2019-06-10 09:19:45 +0530

    Router and Handler Function - Unit Test for Get All items endpoint

--- src/main/java/io/yadnyesh/learnreactivespring/LearnreactivespringApplication.java
+++ src/main/java/io/yadnyesh/learnreactivespring/LearnreactivespringApplication.java
@@ -9,5 +9,4 @@ public class LearnreactivespringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LearnreactivespringApplication.class, args);
 	}
-
 }
---
+++ src/test/java/io/yadnyesh/learnreactivespring/handler/ItemHandlerTest.java
@@ -0,0 +1,166 @@
+package io.yadnyesh.learnreactivespring.handler;
+
+import io.yadnyesh.learnreactivespring.constants.ItemConstants;
+import io.yadnyesh.learnreactivespring.domain.Item;
+import io.yadnyesh.learnreactivespring.repository.ItemReactiveRepository;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@DirtiesContext
+@AutoConfigureWebTestClient
+@ActiveProfiles("test")
+public class ItemHandlerTest {
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    public List<Item> data() {
+        return Arrays.asList(new Item(null, "Samsung TV", 400.0),
+                new Item(null, "LG TV", 500.0),
+                new Item(null, "Apple Watch", 299.99),
+                new Item(null, "Beats Headphones", 149.99),
+                new Item("ABC", "Bose Headphones", 149.99));
+    }
+
+    @Before
+    public void setup() {
+        itemReactiveRepository.deleteAll().thenMany(Flux.fromIterable(data()))
+                .flatMap(itemReactiveRepository::save)
+                .doOnNext(item -> {
+                    System.out.println("Inserted item is: " + item);
+                })
+                .blockLast();
+    }
+
+    @Test
+    public void getAllItems() {
+        webTestClient.get().uri(ItemConstants.ITEM_FUNCTIONAL_END_POINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Item.class)
+                .hasSize(5);
+    }
+
+//    @Test
+//    public void getAllItems_approach2() {
+//        webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1)
+//                .exchange()
+//                .expectStatus().isOk()
+//                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+//                .expectBodyList(Item.class)
+//                .hasSize(5)
+//                .consumeWith((response) ->{
+//                    List<Item> itemList = response.getResponseBody();
+//                    itemList.forEach((item) -> {
+//                        Assert.assertTrue(item.getId() != null);
+//                    });
+//                });
+//    }
+//
+//    @Test
+//    public void getAllItems_approach3() {
+//        Flux<Item> itemFlux = webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1)
+//                .exchange()
+//                .expectStatus().isOk()
+//                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+//                .returnResult(Item.class)
+//                .getResponseBody();
+//
+//        StepVerifier.create(itemFlux.log("Item from Flux: "))
+//                .expectNextCount(5)
+//                .verifyComplete();
+//    }
+//
+//    @Test
+//    public void getOneItem() {
+//        webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1.concat("/{id}"), "ABC")
+//                .exchange()
+//                .expectStatus().isOk()
+//                .expectBody()
+//                .jsonPath("$.price", 149.99);
+//    }
+//
+//    @Test
+//    public void getOneItem_NotFound() {
+//        webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1.concat("/{id}"), "YAD")
+//                .exchange()
+//                .expectStatus().isNotFound();
+//    }
+//
+//    @Test
+//    public void createItem() {
+//        Item item = new Item(null, "Iphone X", 999.99);
+//        webTestClient.post().uri(ItemConstants.ITEM_END_POINT_V1)
+//                .contentType(MediaType.APPLICATION_JSON_UTF8)
+//                .body(Mono.just(item), Item.class)
+//                .exchange()
+//                .expectStatus().isCreated()
+//                .expectBody()
+//                .jsonPath("$.id").isNotEmpty()
+//                .jsonPath("$.description").isEqualTo("Iphone X")
+//                .jsonPath("$.price").isEqualTo(999.99);
+//
+//
+//    }
+//
+//    @Test
+//    public void deleteItem() {
+//        webTestClient.delete().uri(ItemConstants.ITEM_END_POINT_V1.concat("/{id}"), "ABC")
+//                .accept(MediaType.APPLICATION_JSON)
+//                .exchange()
+//                .expectStatus().isOk()
+//                .expectBody(Void.class);
+//
+//    }
+//
+//    @Test
+//    public void updateItem() {
+//        double newPrice = 1000.00;
+//        Item itemToUpdate = new Item(null, "Beats Headphones", newPrice);
+//
+//        webTestClient.put().uri(ItemConstants.ITEM_END_POINT_V1.concat("/{id}"), "ABC")
+//                .contentType(MediaType.APPLICATION_JSON_UTF8)
+//                .accept(MediaType.APPLICATION_JSON_UTF8)
+//                .body(Mono.just(itemToUpdate), Item.class)
+//                .exchange()
+//                .expectStatus().isOk()
+//                .expectBody()
+//                .jsonPath("$.price", newPrice);
+//
+//    }
+//
+//    @Test
+//    public void updateItemNotFound() {
+//        double newPrice = 1000.00;
+//        Item itemToUpdate = new Item(null, "Beats Headphones", newPrice);
+//
+//        webTestClient.put().uri(ItemConstants.ITEM_END_POINT_V1.concat("/{id}"), "YAD")
+//                .contentType(MediaType.APPLICATION_JSON_UTF8)
+//                .accept(MediaType.APPLICATION_JSON_UTF8)
+//                .body(Mono.just(itemToUpdate), Item.class)
+//                .exchange()
+//                .expectStatus().isNotFound();
+//
+//    }
+}
